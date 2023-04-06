79c8cd645b8e7dcabde875d76ab84c3e0c9d6522
Michelle Kim <74822499+Kimmingeyung@users.noreply.github.com>
2023-04-06 17:57:59 +0900

    Add files via upload

---
+++ test1/src/main/java/com/javaoracle/project/web/NowServlet.java
@@ -0,0 +1,35 @@
+package com.javaoracle.project.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/now")
+public class NowServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = "±è¹Î°æ";
+		Date date = new Date();
+		
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>ÀÌ¸§:"+ name+"</h1>");
+		out.println("<h2>ÇöÀç½Ã°£:"+date+"</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+	
+	
+	
+}
